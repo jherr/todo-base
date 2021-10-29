bbfe2f6984df532e1339843e38d66d9c8ffdca3f
hadichloun <hadchloun@hadis-MBP.lan>
2021-10-28 21:43:07 -0500

    adds remoeTodo function

--- src/store.ts
+++ src/store.ts
@@ -1,26 +1,7 @@
-// Standard interface and functions
-export interface Todo {
-  id: number;
-  text: string;
-  done: boolean;
-}
-
-export const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
-  todos.map((todo) => ({
-    ...todo,
-    text: todo.id === id ? text : todo.text,
-  }));
-
-export const toggleTodo = (todos: Todo[], id: number): Todo[] =>
-  todos.map((todo) => ({
-    ...todo,
-    done: todo.id === id ? !todo.done : todo.done,
-  }));
-
-export const removeTodo = (todos: Todo[], id: number): Todo[] =>
+ const removeTodo = (todos: Todo[], id: number): Todo[] =>
   todos.filter((todo) => todo.id !== id);
 
-export const addTodo = (todos: Todo[], text: string): Todo[] => [
+ const addTodo = (todos: Todo[], text: string): Todo[] => [
   ...todos,
   {
     id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
@@ -28,3 +9,4 @@ export const addTodo = (todos: Todo[], text: string): Todo[] => [
     done: false,
   },
 ];
+
