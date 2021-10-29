f38a143a71eafb4c4028ac6bb670fc3bcea15672
hadichloun <hadchloun@hadis-MBP.lan>
2021-10-28 21:46:24 -0500

    adds delete function and tested code multiple times

--- src/store.ts
+++ src/store.ts
@@ -22,27 +22,3 @@ const addTodo = (todos: Todo[], text: string): Todo[] => [
 },
 ];
 
-const removeTodo = (todos: Todo[], id: number): Todo[] =>
-todos.filter((todo) => todo.id !== id);
-
-const addTodo = (todos: Todo[], text: string): Todo[] => [
-...todos,
-{
-  id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
-  text,
-  done: false,
-},
-];
-
-const removeTodo = (todos: Todo[], id: number): Todo[] =>
-todos.filter((todo) => todo.id !== id);
-
-const addTodo = (todos: Todo[], text: string): Todo[] => [
-...todos,
-{
-  id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
-  text,
-  done: false,
-},
-];
-
