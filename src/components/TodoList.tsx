2594e1479648697ccfb10934a615d869e1fb6873
hadi chloun <hadi_960@hotmail.com>
2021-11-02 21:31:18 -0500

    maps thru todo and onChange the eventlistner

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,25 +1,43 @@
 import * as React from "react";
 import { Button, Input, Flex, Checkbox, Heading } from "@chakra-ui/react";
+import { observer } from "mobx-react";
+
+import store from "../store";
 
 function TodoListItems() {
   return (
     <>
-      {[].map((todo: { id: number; text: string }) => (
+      {store.todos.map((todo) => (
         <Flex pt={2} key={todo.id}>
-          <Checkbox />
-          <Input mx={2} value={todo.text} />
-          <Button>Delete</Button>
+          <Checkbox
+            onClick={() => (todo.done = !todo.done)}
+            checked={todo.done}
+          />
+          <Input
+            mx={2}
+            value={todo.text}
+            onChange={(evt) => (todo.text = evt.target.value)}
+          />
+          <Button
+            onClick={() => {
+              store.removeTodo(todo.id);
+            }}
+          >
+            Delete
+          </Button>
         </Flex>
       ))}
     </>
   );
 }
 
+const ObservedTodoListItems = observer(TodoListItems);
+
 function TodoList() {
   return (
     <>
       <Heading>Todo List</Heading>
-      <TodoListItems />
+      <ObservedTodoListItems />
     </>
   );
 }
